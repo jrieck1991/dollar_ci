8f378dac2bcd0ef5d16e2911d84ff6db8261ad03
jrieck <jack.rieck@sendgrid.com>
2020-04-17 08:33:33 -0600

    rust http handlers (#3)
    * rust http handlers
    
    * newline

---
+++ http_handlers/src/main.rs
@@ -0,0 +1,11 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+
+    let events = warp::post()
+        .and(warp::path("events")
+        .map(|| "Hello" ));
+    
+    warp::serve(events).run(([0, 0, 0, 0], 8080)).await;
+}
