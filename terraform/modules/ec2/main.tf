d99bb16544032491d57289970f5f5fdc5146c643
Jack Rieck <jack.rieck@sendgrid.com>
2020-04-16 19:02:48 -0600

    update role

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -49,7 +49,7 @@ EOF
 
 // get managed aws SSM policy
 data "aws_iam_policy" "ssm" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "asg" {
