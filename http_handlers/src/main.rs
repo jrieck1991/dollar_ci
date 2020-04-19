253e9a694b5b9431a8062ee340b9a09210ebb68c
Jack Rieck <jack.rieck@sendgrid.com>
2020-04-19 16:44:01 -0600

    stubbed out most methods

--- http_handlers/src/main.rs
+++ http_handlers/src/main.rs
@@ -1,13 +1,91 @@
 use warp::Filter;
 
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Event {
+    action: String,
+    check_suite: Check_Suite,
+    app: App,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Check_Suite {
+    id: u64,
+    status: String,
+    head_sha: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct App {
+    id: u64,
+}
+
 #[tokio::main]
 async fn main() {
 
     let events = warp::post()
-        .and(warp::path("events")
-        .map(|| "Hello" ));
+        .and(warp::body::json())
+        .map(|action, mut event: Event|  {
+            
+        });
     
     warp::serve(events).run(([0, 0, 0, 0], 80)).await;
+}
+
+// requested listenes for actions of 'requested' and 'rerequested'
+fn requested() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("events")
+        .and(warp::post())
+        .and(json_body())
+}
+
+// created listens for 'check_run' 'created' events
+fn created() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("events")
+        .and(warp::post())
+        .and(json_body())
+}
+
+// assert body is json and within a size limit
+fn json_body() -> impl Filter<Extract = (Event,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+// create jwt from pem
+fn get_jwt() -> String {
+    let jwt = String::from("JWT STRING HERE")
+}
+
+// tell github to create 'check_run'
+fn check_run_create(name: String, head_sha: String) {
+    let client = reqwest::Client::new();
+    let res = client.post("http://somewhere.com")
+        .json("body")
+        .send()
+        .await?;
+}
+
+// update 'check_run' to 'in progress'
+fn check_run_start(check_id: String) {
+    let client = reqwest::Client::new();
+    let res = client.post("http://somewhere.com")
+        .json("body")
+        .send()
+        .await?;
+}
+
+// mark check_run as complete
+fn check_run_complete(check_id: String, success: bool) {
+    let client = reqwest::Client::new();
+    let res = client.post("http://somewhere.com")
+        .json("body")
+        .send()
+        .await?;
+}
+
+// clone head_sha of git branch
+fn clone(head_sha: String) {
 }
 
 // listen for 'check_suite' of type 'requested', this means new code is pushed to a repo
