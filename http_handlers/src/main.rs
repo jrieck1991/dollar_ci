684e6a14386d579c92a9cc66b1b4f54ca31efa67
Jack Rieck <jack.rieck@sendgrid.com>
2020-04-19 14:41:08 -0600

    update terraform modules

--- http_handlers/src/main.rs
+++ http_handlers/src/main.rs
@@ -9,3 +9,13 @@ async fn main() {
     
     warp::serve(events).run(([0, 0, 0, 0], 80)).await;
 }
+
+// listen for 'check_suite' of type 'requested', this means new code is pushed to a repo
+// 'rerequested' means the user manually requested a re run of the check
+// create JWT from pem to authenticate https requests to github
+// create a 'check_run' next
+// github will create a 'check_run' and send back a 'created' action
+// update 'check_run' to in progress
+// use 'x-access-token' to clone repo
+// run CI
+// make 'check_run' as complete
